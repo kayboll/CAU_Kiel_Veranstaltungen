ef6855717e1af36df2fb6460a8c45b4a61cd19a9
Kay Boll <kay@bollcloud.de>
2019-12-16 17:40:27 +0100

    get Title and event time

--- scraper.py
+++ scraper.py
@@ -3,19 +3,34 @@
 
 import scraperwiki
 import lxml.html
+import re
 #
 # # Read in a page
 html = scraperwiki.scrape("http://www.uni-kiel.de/de/veranstaltungen/ueberblick")
 #
 # # Find something on the page using css selectors
 root = lxml.html.fromstring(html)
-root.cssselect("div[align='left']")
-#
+events = root.cssselect("div[class='news-list-item']")
+for event in events:        # Second Example
+    #elementDatePlaceTime = event.cssselect("div[class='news-list-item__dateline']")[0].text_content().strip().split("|")
+
+    #print 'Date :',elementDatePlaceTime
+    Time = event.cssselect("div[class='news-list-item__dateline']")[0][1].text_content()
+    Time = ''.join(Time.split())
+    Time = Time.split("|")
+    Time = " | ".join(Time)
+    
+    Title = event.cssselect("h3[class='ce-headline-h4']")[0].cssselect("span")[0].text_content()
+    print Title
+    print Time
+    scraperwiki.sqlite.save(unique_keys=['title'], data={"title": Title, "time": Time})
+
+  
 # # Write out to the sqlite database using scraperwiki library
-scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
+#scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
 #
 # # An arbitrary query against the database
-scraperwiki.sql.select("* from data where 'name'='peter'")
+scraperwiki.sql.select("* from data")
 
 # You don't have to do things with the ScraperWiki and lxml libraries.
 # You can use whatever libraries you want: https://morph.io/documentation/python
