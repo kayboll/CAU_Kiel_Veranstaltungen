6a05fe98a414f3c83948c0bcde8ee08b08f1cfaf
Kay Boll <kay@bollcloud.de>
2019-12-16 16:26:33 +0100

    remove spaces

--- scraper.py
+++ scraper.py
@@ -8,14 +8,14 @@ import lxml.html
 html = scraperwiki.scrape("http://www.uni-kiel.de/de/veranstaltungen/ueberblick")
 #
 # # Find something on the page using css selectors
- root = lxml.html.fromstring(html)
- root.cssselect("div[align='left']")
+root = lxml.html.fromstring(html)
+root.cssselect("div[align='left']")
 #
 # # Write out to the sqlite database using scraperwiki library
- scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
+scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
 #
 # # An arbitrary query against the database
- scraperwiki.sql.select("* from data where 'name'='peter'")
+scraperwiki.sql.select("* from data where 'name'='peter'")
 
 # You don't have to do things with the ScraperWiki and lxml libraries.
 # You can use whatever libraries you want: https://morph.io/documentation/python
